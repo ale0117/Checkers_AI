a4298827e9e3f776306185090bb6687c651d5fe3
An Le <ale01172002@gmail.com>
2023-10-28 18:40:33 -0700

    Update StudentAI.py

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -1,6 +1,7 @@
 from random import randint
 from BoardClasses import Move
 from BoardClasses import Board
+import Checker
 #The following part should be completed by students.
 #Students can modify anything except the class name and existing functions and variables.
 class StudentAI():
@@ -16,6 +17,7 @@ class StudentAI():
         self.color = 2
         self.max_depth = 3
         self.color_map = {2:'W', 1:'B'}
+        
 
     def get_move(self,move):
         if len(move) != 0:
@@ -23,6 +25,7 @@ class StudentAI():
         else:
             self.color = 1
         move = self.mini_max_search()
+        self.board.make_move(move, self.color)
         return move
 
     def mini_max_search(self):
@@ -36,15 +39,17 @@ class StudentAI():
         if len(moves) == 0:
             return self.board_score(self.color), None
         res = float('-inf')
-        best_move = None
+        best_move = moves[0][0]
         for move_seq in moves:
             for move in move_seq:
                 self.board.make_move(move, self.color)
-                res = max(res, self.min_val(curr_depth + 1, alpha, beta)[0])
-                if res > alpha:
-                    alpha = res
+                score = self.min_val(curr_depth + 1, alpha, beta)[0]
+                if score >= res:
+                    res = score
                     best_move = move
+                alpha = max(alpha, res)
                 if alpha >= beta:
+                    self.board.undo()
                     return res, best_move
                 self.board.undo()
         return res, best_move
@@ -56,40 +61,112 @@ class StudentAI():
         if len(moves) == 0:
             return self.board_score(self.opponent[self.color]), None
         res = float('inf')
-        best_move = None
+        best_move = moves[0][0]
         for move_seq in moves:
             for move in move_seq:
                 self.board.make_move(move, self.opponent[self.color])
-                res = min(res, self.max_val(curr_depth + 1, alpha, beta)[0])
-                if res < beta:
-                    beta = res
+                score = self.max_val(curr_depth + 1, alpha, beta)[0]
+                if score <= res:
+                    res = score
                     best_move = move
+                alpha = min(alpha, res)
                 if alpha >= beta:
+                    self.board.undo()
                     return res, best_move
                 self.board.undo()
         return res, best_move
 
     def board_score(self, turn):
-        win = self.board.is_win(self.color_map[turn])
-        if win == self.color:
-            return float('inf')
-        elif win == self.opponent[self.color]:
-            return float('-inf')
-        else:
-            score = 0
+        ai_score = 0
+        opponent_score = 0
+        tie_breaker = randint(0, 10)
+        for r in range(self.row):
             for c in range(self.col):
-                for r in range(self.row):
-                    piece = self.board.board[c][r]
-                    if piece.get_color() == self.color:
-                        if piece.is_king:
-                            score += 2
+                piece = self.board.board[r][c]
+                if piece.get_color() == self.color_map[self.color]:
+                    if piece.is_king:
+                        ai_score += 2000
+                        # encourage king to take space in mid
+                        ai_score += ((self.row - 1) / 2 * 100) - (abs((self.row - 1)/ 2 - r) * 100)
+                        ai_score += ((self.col - 1) / 2 * 50) - (abs((self.col -1)/ 2 - c) * 50)
+                    else:
+                        ai_score += 1000
+                        # encourage normal pieces to move towards other end to become king
+                        if self.color != 1:
+                            ai_score += (self.row - r) / self.row * 1000
                         else:
-                            score += 1
-                    elif piece.get_color() == self.opponent[self.color]:
-                        if piece.is_king:
-                            score -= 2
+                            ai_score += (r+1) / self.row * 1000
+                    outside = 0
+                    for x in [(-1, -1), (-1, 1), (1, -1), (1, 1)]:
+                        if not(self.board.is_in_board(r + x[0], c + x[1])):
+                            outside += 1
+                            if outside == 2:
+                                ai_score += 100 # add points for being at edge
+                            if outside == 3:
+                                ai_score -= 300 # subtract points for being at corner
                         else:
-                            score -= 1
-            return score
+                            new_r = r+x[0]
+                            new_c = c+x[1]
+                            another_piece = self.board.board[new_r][new_c]
+                            if another_piece.get_color() == self.color_map[self.color]:
+                                ai_score += 20 # award points for having pieces together (strength in unity)
+                            elif another_piece.get_color() == self.color_map[self.opponent[self.color]]:
+                                #detract points for walking into capture
+                                if self.board.is_in_board(r-x[0], c-x[1]) and self.board.board[r-x[0]][c-x[1]].color == '.' and turn == self.opponent[self.color]:
+                                    if another_piece.is_king:
+                                        ai_score -= 1500
+                                    else:
+                                        if self.color == 1 and new_r > r:
+                                            ai_score -= 1500
+                                        elif self.color == 2 and new_r < r:
+                                            ai_score -= 1500           
+                elif piece.get_color() == self.color_map[self.opponent[self.color]]:
+                    if piece.is_king:
+                        opponent_score += 2000
+                        # encourage king to take space in mid
+                        opponent_score += ((self.row - 1) / 2 * 100) - (abs((self.row - 1)/ 2 - r) * 100)
+                        opponent_score += ((self.col - 1) / 2 * 50) - (abs((self.col -1)/ 2 - c) * 50)
+                    else:
+                        opponent_score += 1000
+                        # encourage normal pieces to move towards other end to become king
+                        if self.opponent[self.color] != 1:
+                            opponent_score += (self.row - r) / self.row * 1000
+                        else:
+                            opponent_score += (r+1) / self.row * 1000
+                        
+                    outside = 0
+                    for x in [(-1, -1), (-1, 1), (1, -1), (1, 1)]:
+                        if not(self.board.is_in_board(r + x[0], c + x[1])):
+                            outside += 1
+                            if outside == 2:
+                                opponent_score += 100 # add points for being at edge
+                            if outside == 3:
+                                opponent_score -= 300 # subtract points for being at corner
+                        else:
+                            new_r = r+x[0]
+                            new_c = c+x[1]
+                            another_piece = self.board.board[new_r][new_c]
+                            if another_piece.get_color() == self.color_map[self.opponent[self.color]]:
+                                opponent_score += 20 # award points for having pieces together (strength in unity)
+                            elif another_piece.get_color() == self.color_map[self.color]:
+                                #detract points for walking into capture
+                                if self.board.is_in_board(r-x[0], c-x[1]) and self.board.board[r-x[0]][c-x[1]].color == '.' and turn == self.color:
+                                    if another_piece.is_king:
+                                        opponent_score -= 1500
+                                    else:
+                                        if self.opponent[self.color] == 1 and new_r > r:
+                                            opponent_score -= 1500
+                                        elif self.opponent[self.color] == 2 and new_r < r:
+                                            opponent_score -= 1500
+        # award points for having more amount of pieces (strength in numbers)
+        if self.color == 1:
+            opponent_score  += self.board.white_count / (self.col * self.p / 2) * 1000 
+            ai_score += self.board.black_count / (self.col * self.p / 2) * 1000
+        else:
+            ai_score += self.board.white_count / (self.col * self.p / 2) * 1000
+            opponent_score += self.board.black_count / (self.col * self.p / 2) * 1000
+        return ai_score - opponent_score + tie_breaker
 
 
+if __name__ == "__main__":
+    pass
